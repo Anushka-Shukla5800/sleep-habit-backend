5c071012e990d88a72c7388709a3bfb116b46ae8
Anushka Shukla <anushka@Anushkas-MacBook-Air.local>
2026-02-04 11:54:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/anushka/sleep_app_backend/SleepAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anushka.sleep_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/anushka/sleep_app_backend/controller/Controller.java
@@ -0,0 +1,8 @@
+package com.anushka.sleep_app_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+}
